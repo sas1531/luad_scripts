6ae1cc03e6b2ae1cec782bc244076309e059e04c
Shaleigh Smith <Shaleigh.Smith@nyumc.org>
2019-02-28 17:47:58 -0500

    Update Pipeline

--- luad_analysis_pipeline.R
+++ luad_analysis_pipeline.R
@@ -1,11 +1,12 @@
 #!/Applications/anaconda3/pkgs/r-base-3.4.3-h9b62496_0/bin/Rscript
 
 library(tidyverse)
+library(plyr)
 library(dplyr)
 library(ggplot2)
 library(optparse, lib.loc="/Library/Frameworks/R.framework/Versions/3.5/Resources/library")
 library(gridExtra, lib.loc="/Library/Frameworks/R.framework/Versions/3.5/Resources/library")
-library(plyr)
+
 
 options(warn=1)
 
@@ -26,7 +27,10 @@ option_list = list(
   make_option(c("-u", "--upper_x_lime"), type="integer", default="20", 
               help="upper x limit for distribution plot", metavar="character"),
   make_option(c("-l", "--lower_x_lim"), type="integer", default="-20", 
-              help="lower x limit for distribution plot", metavar="character")
+              help="lower x limit for distribution plot", metavar="character"),
+  make_option(c("-t", "--tag_phospho"), type="character", default="", 
+              help="tag for phospho data: write 'phospho' if the data is phosphoproteome 
+              and 'not_phospho' if it isn't", metavar="character")
 ); 
 
 opt_parser = OptionParser(option_list=option_list);
@@ -75,8 +79,10 @@ moveme <- function (invec, movecommand) {
 
 
 # test script:
-# Rscript luad_analysis_pipeline_test.R -f luad-v1.0-phosphodfeome-ratio-norm-NArm.gct -r 27 -c 23 -m 23 -o xxxxxxxx
-
+# Rscript luad_analysis_pipeline_test.R -f ./luad_original_data/luad-v1.0-phosphoproteome-ratio-norm-NArm.gct -r 27 -c 24 -m 23 -o xxxxxxxx -t phospho
+# Rscript luad_analysis_pipeline_test.R -f ./luad_original_data/luad-v1.0-proteome-ratio-norm-NArm.gct -r 27 -c 18 -m 17 -o xxxxxxxx -t not_phospho
+# Rscript luad_analysis_pipeline_test.R -f ./luad_original_data/luad-v1.0-rnaseq-linear-gene-fpkm-uq-log10-NArm-row-norm.gct -r 22 -c 6 -m 4 -o xxxxxxxx -t not_phospho
+# Rscript luad_analysis_pipeline_test.R -f ./luad_original_data/luad-v1.0-wgs-cnv-somatic.luad.all.hg38-v2.0-20190121.gct -r 22 -c 6 -m 2 -o xxxxxxxx -t not_phospho
 
 ### Establish names 
 out_dataframe <- paste(opt$o, ".tsv", sep="")
@@ -93,8 +99,8 @@ ggplot_title <- paste(opt$o, "Distribution", sep=" ")
 # Isolate only the gene ID and sample columns
 # Export this as a new dataframe 
 df <- read.table(opt$f, skip = 2, header = TRUE, sep = "\t", fill = TRUE)
+colnames(df)[opt$m] <- "GeneSymbol"
 df_gene <- df[c(opt$r:nrow(df)), c(1, opt$m, opt$c:ncol(df))]
-as.tibble(df_gene)
 write.table(df_gene, file=out_dataframe, quote=FALSE, sep='\t', col.names = NA)
 
 # Get a list of sample names for the outlier analysis
@@ -128,10 +134,16 @@ dev.off()
 
 # Create dataframe without gene ids and one with only gene ids
 df_stats <- df_gene[,4:ncol(df_gene)]
-df_genesymbol <- df_gene[,1:2]
-df_genesymbol <- as.data.frame(df_genesymbol)
-as.tibble(df_genesymbol)
-as.tibble(df_stats)
+df_genesymbol <- dplyr::select(df_gene, GeneSymbol)
+
+########
+
+
+
+####### CHANGE GENESYMBOL IN COMMAND INPUT
+
+
+#######
 
 # Change all values to numeric
 df_stats[] <- lapply(df_stats, function(x) as.numeric(as.character(x)))
@@ -157,53 +169,81 @@ df_outlier_plus <- apply(df_stats, 2, function(x) ifelse(x > df_stats$row_med_pl
 df_outlier_minus <- apply(df_stats, 2, function(x) ifelse(x < df_stats$row_med_minus, 1, 0))
 
 # Replace the outliers above 1.5*iqr with 1 and replace the outliers below 1.5*iqr with -1, all else replace with zero
-df_outlier_both <- apply(df_stats, 2, function(x) ifelse(x > df_stats$row_med_plus, 1, ifelse(x < df_stats$row_med_minus, -1, 0)))
-
-#df_compare_outliers <- read.table('luad_df_outliers.tsv', header = TRUE, sep = "\t", fill = TRUE)
-#as.tibble(df_outlier_plus)
-#as.tibble(df_outlier_minus)
-#as.tibble(df_outlier_both)
-#as.tibble(df_compare_outliers)
+#df_outlier_both <- apply(df_stats, 2, function(x) ifelse(x > df_stats$row_med_plus, 1, ifelse(x < df_stats$row_med_minus, -1, 0)))
 
 # Remove the calculation columns and add a count column to the plus/minus dataframes
 df_outlier_plus <- df_outlier_plus[,-c(((ncol(df_outlier_plus))-3):ncol(df_outlier_plus))]
 df_outlier_plus <- cbind(df_outlier_plus, count = rowSums(df_outlier_plus))
 df_outlier_minus <- df_outlier_minus[,-c(((ncol(df_outlier_minus))-3):ncol(df_outlier_minus))]
 df_outlier_minus <- cbind(df_outlier_minus, count = rowSums(df_outlier_minus))
-df_outlier_both <- df_outlier_both[,-c(((ncol(df_outlier_both))-3):ncol(df_outlier_both))]
-df_outlier_both <- cbind(df_outlier_both, count = rowSums(df_outlier_both))
+#df_outlier_both <- df_outlier_both[,-c(((ncol(df_outlier_both))-3):ncol(df_outlier_both))]
+#df_outlier_both <- cbind(df_outlier_both, count = rowSums(df_outlier_both))
 
 # Bind dataframe with original gene IDs
 df_outlier_gene_plus <- cbind(df_genesymbol, df_outlier_plus)
 df_outlier_gene_minus <- cbind(df_genesymbol, df_outlier_minus)
-df_outlier_gene_both <- cbind(df_genesymbol, df_outlier_both)
+#df_outlier_gene_both <- cbind(df_genesymbol, df_outlier_both)
 
-# Aggregate phosphosites and dfeins
-gene_symbol <- df_outlier_gene_both[,2:2]
-df_outlier_plus_agg <- aggregate(. ~ gene_symbol, data = df_outlier_gene_plus, sum, na.action = na.pass)
-df_outlier_minus_agg <- aggregate(. ~ gene_symbol, data = df_outlier_gene_minus, sum, na.action = na.pass)
-df_outlier_both_agg <- aggregate(. ~ gene_symbol, data = df_outlier_gene_both, sum, na.action = na.pass)
+# Aggregate dfdfites 
+count_not_outliers <-  function(x){ length(which(x==0))}
+count_outliers <- function(x){ length(which(x==1))}
 
-#Reorder columns so that id is first
-df_outlier_plus_agg <- df_outlier_plus_agg[moveme(names(df_outlier_plus_agg), "id first")]
-df_outlier_minus_agg <- df_outlier_minus_agg[moveme(names(df_outlier_minus_agg), "id first")]
-df_outlier_both_agg <- df_outlier_both_agg[moveme(names(df_outlier_both_agg), "id first")]
+# Plus
+df_final_outlier_plus <- dplyr::select(df_outlier_gene_plus, -count)
+colnames(df_final_outlier_plus) <- paste(colnames(df_final_outlier_plus), "not.outlier", sep = ".")
+colnames(df_final_outlier_plus)[1] <- "GeneSymbol"
+df_final_not_outlier_plus <- dplyr::select(df_outlier_gene_plus, -count)
+colnames(df_final_not_outlier_plus) <- paste(colnames(df_final_not_outlier_plus), "outlier", sep = ".")
+colnames(df_final_not_outlier_plus)[1] <- "GeneSymbol.not"
+
+# Minus
+df_final_outlier_minus <- dplyr::select(df_outlier_gene_minus, -count)
+colnames(df_final_outlier_minus) <- paste(colnames(df_final_outlier_minus), "not.outlier", sep = ".")
+colnames(df_final_outlier_minus)[1] <- "GeneSymbol"
+df_final_not_outlier_minus <- dplyr::select(df_outlier_gene_minus, -count)
+colnames(df_final_not_outlier_minus) <- paste(colnames(df_final_not_outlier_minus), "outlier", sep = ".")
+colnames(df_final_not_outlier_minus)[1] <- "GeneSymbol.not"
+
+agg_phospho <- function(df_outlier_agg, df_not_outlier_agg, tag){
+  if (tag == 'phospho'){
+    # Plus
+    df_final_outlier_agg <- aggregate(. ~ GeneSymbol, data = df_outlier_agg, 
+                                       count_outliers, na.action = na.pass)
+    df_final_not_outlier_agg <- aggregate(. ~ GeneSymbol.not, data = df_not_outlier_agg, 
+                                           count_not_outliers, na.action = na.pass)
+    df_final_outlier <- cbind(df_final_outlier_agg, df_final_not_outlier_agg)
+    df_final_outlier <- dplyr::select(df_final_outlier, -GeneSymbol.not)
+    df_final_outlier$GeneSymbol <- as.factor(as.character(df_final_outlier$GeneSymbol))
+    return(df_final_outlier)
+  } else 
+    df_final_outlier <- cbind(df_outlier_agg, df_not_outlier_agg)
+    df_final_outlier <- dplyr::select(df_final_outlier, -GeneSymbol.not)
+    return(df_final_outlier)
+}
+
+
+write.table(df_outlier_gene_plus, file = 'yyyy.tsv', quote=FALSE, sep='\t', col.names = NA)
+
+
+# Run aggregation function
+df_final_plus <- agg_phospho(df_final_outlier_plus ,df_final_not_outlier_plus, opt$t)
+df_final_minus <- agg_phospho(df_final_outlier_minus ,df_final_not_outlier_minus, opt$t)
 
 # Write out outlier dataframes
-write.table(df_outlier_plus_agg, out_outlier_plus, quote=FALSE, sep='\t', col.names = NA)
-write.table(df_outlier_minus_agg, out_outlier_minus, quote=FALSE, sep='\t', col.names = NA)
-write.table(df_outlier_both_agg, out_outlier_both, quote=FALSE, sep='\t', col.names = NA)
+write.table(df_final_plus, file = out_outlier_plus, quote=FALSE, sep='\t', col.names = NA)
+write.table(df_final_minus, file = out_outlier_minus, quote=FALSE, sep='\t', col.names = NA)
+#write.table(df_outlier_both_agg, out_outlier_both, quote=FALSE, sep='\t', col.names = NA)
 
 
 ### Group Comparison
 
 # Add Count column filled with NAs to the end of the meta dataframe
-df_meta$Count <- NA
+#df_meta$Count <- NA
 
 # Combine outlier dataframes with meta data
-meta_outlier_plus <- rbind.fill(df_meta, df_outlier_plus_agg)
-meta_outlier_minus <- rbind.fill(df_meta, df_outlier_minus_agg)
-meta_outlier_both <- rbind.fill(df_meta, df_outlier_both_agg)
+#meta_outlier_plus <- rbind.fill(df_meta, df_outlier_plus_agg)
+#meta_outlier_minus <- rbind.fill(df_meta, df_outlier_minus_agg)
+#meta_outlier_both <- rbind.fill(df_meta, df_outlier_both_agg)
 
 # Call group dataframes
 
